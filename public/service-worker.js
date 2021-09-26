39a910a5b201e00e361ce69dc72e413894ed5ae6
spreston <sam.preston11@gmail.com>
2021-09-26 16:00:53 -0500

    files cached in service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,14 @@
 // Add files to cache
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/db.js'
+];
 
 // Static & Data cahce
 
