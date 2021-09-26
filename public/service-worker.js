53282d2850a979253510bb07ecf4af3fa9d6221c
spreston <sam.preston11@gmail.com>
2021-09-26 16:05:37 -0500

    Service worker installs.

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,8 +11,18 @@ const FILES_TO_CACHE = [
 ];
 
 // Static & Data cahce
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 // Install service worker
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(caches.open(CACHE_NAME).then(cache => {
+        console.log('Your files were pre-cached successfully!');
+        return cache.addAll(FILES_TO_CACHE);
+    }));
+
+    self.skipWaiting();
+});
 
 // Active service worker
 
